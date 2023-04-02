75c7eb2da0a812b1f68e915ffb8be1ee9cf1d1e0
Andrew <andrewdp56@outlook.com>
2023-04-02 14:08:37 -0400

    Initial Commit

---
+++ S1/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace S1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+
+            Console.WriteLine("What is your name?");
+
+            name = Console.ReadLine() + ".";
+
+            while (true)
+            {
+                Console.Clear();
+
+                Console.WriteLine("Welcome " + name);
+                Console.WriteLine();
+                Console.WriteLine("1. Add two numbers");
+                Console.WriteLine("2. Multiply two numbers");
+                Console.WriteLine("3. Subtract two numbers");
+                Console.WriteLine("4. Divide two numbers");
+
+
+                bool optionIsValid = false;
+                int option = 1;
+
+                while(optionIsValid == false)
+                {
+                    try
+                    {
+                        option = Convert.ToInt32(Console.ReadLine());
+                        optionIsValid = true;
+                    }
+                    catch { }
+                }
+                
+
+
+                int firstNum;
+                int secondNum;
+
+                if (option == 1)
+                {
+                    Console.Write("First Number: ");
+                    firstNum = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Second Number: ");
+                    secondNum = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Result: " + (firstNum + secondNum));
+                }
+                else if (option == 2)
+                {
+                    Console.Write("First Number: ");
+                    firstNum = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Second Number: ");
+                    secondNum = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Result: " + (firstNum * secondNum));
+                }
+                else if (option == 3)
+                {
+                    Console.Write("First Number: ");
+                    firstNum = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Second Number: ");
+                    secondNum = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Result: " + (firstNum - secondNum));
+                }
+                else
+                {
+                    Console.Write("First Number: ");
+                    firstNum = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Second Number: ");
+                    secondNum = Convert.ToInt32(Console.ReadLine());
+
+                    Console.Write("Result: " + (firstNum / secondNum));
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
